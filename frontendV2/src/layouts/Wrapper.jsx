fdc820852d4ccae0402fb4f8b63d7286b08b5298
Adeoye Temitayo <adeoyetemitayo99@gmail.com>
2024-08-31 00:39:10 +0100

    update route

--- frontendV2/src/layouts/Wrapper.jsx
+++ frontendV2/src/layouts/Wrapper.jsx
@@ -1,23 +1,27 @@
-import { Link, Outlet, useNavigate } from 'react-router-dom'
-import { ClerkProvider, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-    throw new Error("Missing Publishable Key")
+  throw new Error("Missing Publishable Key");
 }
 
 export default function Wrapper() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <ClerkProvider
-            routerPush={(to) => navigate(to)}
-            routerReplace={(to) => navigate(to, { replace: true })}
-            publishableKey={PUBLISHABLE_KEY}
-        >
-
-            <Outlet />
-        </ClerkProvider>
-    )
+  return (
+    <ClerkProvider
+      routerPush={(to) => navigate(to)}
+      routerReplace={(to) => navigate(to, { replace: true })}
+      publishableKey={PUBLISHABLE_KEY}
+    >
+      <Outlet />
+    </ClerkProvider>
+  );
 }
---
+++ frontendV2/src/route/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./routes/HomePage.jsx";
+import Dashboard from "./routes/Dashboard.jsx";
+import ChatPage from "./routes/ChatPage.jsx";
+import Notfound from "./routes/Notfound.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
+import Wrapper from "./layouts/Wrapper.jsx";
+import Signup from "./routes/Signup.jsx";
+import Login from "./routes/Login.jsx";
+
+
+const router = createBrowserRouter([
+  {
+    element: <Wrapper />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/sign-in/*", element: <Login /> },
+      { path: "/sign-up/*", element: <Signup /> },
+      {
+        element: <MainLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <Dashboard />,
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <ChatPage />,
+          },
+        ],
+      },
+      {
+        path: "*",
+        element: <Notfound />,
+      },
+    ],
+  },
+]);
+
+export default router;
